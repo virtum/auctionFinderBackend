63eeb37768fabef338a84e02604655444832c664
Virtum <pawel.filocha@gmail.com>
2017-05-22 21:00:52 +0200

    Removed redundant code and file

--- src/main/java/com/filocha/Program.java
+++ src/main/java/com/filocha/Program.java
@@ -1,5 +1,8 @@
 package com.filocha;
 
+import com.filocha.messaging.messages.finder.ItemFinderRequestMessage;
+import com.filocha.messaging.messages.finder.ItemFinderResponseMessage;
+import com.filocha.messaging.server.ServerBusImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,17 @@ public class Program {
 
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Program.class, args);
+        ServerBusImpl serverBus = new ServerBusImpl();
+        //serverBus.setConsumerAndProducer("failover://tcp://192.168.99.100:61616");
+        serverBus.setConsumerAndProducer("failover://tcp://localhost:61616");
+        serverBus.addHandler(it -> {
+            ItemFinderResponseMessage response = new ItemFinderResponseMessage();
+            response.setResponse(it.getItemName() + " :work");
+            System.out.println(it.getItemName());
+            return response;
+        }, ItemFinderRequestMessage.class, ItemFinderResponseMessage.class);
+
+
     }
 
 }
