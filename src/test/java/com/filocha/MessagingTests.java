826839c807a8415ab9dd1f8ba4522e2240cad465
Filocha <pafo@gft.com>
2017-05-26 11:14:36 +0200

    Replace env variable in test with hardcoded url

--- src/test/java/com/filocha/MessagingTests.java
+++ src/test/java/com/filocha/MessagingTests.java
@@ -4,7 +4,6 @@ import com.filocha.messaging.client.ClientBusImpl;
 import com.filocha.messaging.server.ServerBusImpl;
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -14,19 +13,18 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class MessagingTests {
 
-    @Value("${dockerPort}")
-    private String dockerPort;
-
     @Test
     public void shouldSendAndReceiveMessageViaActiveMQ() throws ExecutionException, InterruptedException {
         BasicConfigurator.configure();
 
         ServerBusImpl serverBus = new ServerBusImpl();
-        serverBus.setConsumerAndProducer(dockerPort);
+        serverBus.setConsumerAndProducer("failover://tcp://192.168.99.100:61616");
+        //serverBus.setConsumerAndProducer("failover://tcp://localhost:61616");
         serverBus.addHandler(it -> it + " world", String.class, String.class);
 
         ClientBusImpl clientBus = new ClientBusImpl();
-        clientBus.setConsumerAndProducer(dockerPort);
+        clientBus.setConsumerAndProducer("failover://tcp://192.168.99.100:61616");
+        //clientBus.setConsumerAndProducer("failover://tcp://localhost:61616");
         CompletableFuture<String> future = clientBus.sendRequest("hello", String.class);
 
         String response = future.get();
