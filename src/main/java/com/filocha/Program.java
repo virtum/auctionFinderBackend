c3575c97565668d858626b90282d9437c8ccb527
Filocha <pafo@gft.com>
2017-05-26 11:07:35 +0200

    Add new environmental variable to avoid swapping of docker port

--- src/main/java/com/filocha/Program.java
+++ src/main/java/com/filocha/Program.java
@@ -3,25 +3,32 @@ package com.filocha;
 import com.filocha.messaging.messages.finder.ItemFinderRequestMessage;
 import com.filocha.messaging.messages.finder.ItemFinderResponseMessage;
 import com.filocha.messaging.server.ServerBusImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Program {
 
-    public static void main(String[] args) throws InterruptedException {
-        SpringApplication.run(Program.class, args);
+    @Value("${dockerPort}")
+    private String dockerPort;
+
+    @PostConstruct
+    public void startServerBus() {
         ServerBusImpl serverBus = new ServerBusImpl();
-        //serverBus.setConsumerAndProducer("failover://tcp://192.168.99.100:61616");
-        serverBus.setConsumerAndProducer("failover://tcp://localhost:61616");
+        serverBus.setConsumerAndProducer(dockerPort);
         serverBus.addHandler(it -> {
             ItemFinderResponseMessage response = new ItemFinderResponseMessage();
             response.setResponse(it.getItemName() + " :work");
             System.out.println(it.getItemName());
             return response;
         }, ItemFinderRequestMessage.class, ItemFinderResponseMessage.class);
+    }
 
-
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Program.class, args);
     }
 
 }
--- src/test/java/com/filocha/MessagingTests.java
+++ src/test/java/com/filocha/MessagingTests.java
@@ -4,6 +4,7 @@ import com.filocha.messaging.client.ClientBusImpl;
 import com.filocha.messaging.server.ServerBusImpl;
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -13,18 +14,19 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class MessagingTests {
 
+    @Value("${dockerPort}")
+    private String dockerPort;
+
     @Test
     public void shouldSendAndReceiveMessageViaActiveMQ() throws ExecutionException, InterruptedException {
         BasicConfigurator.configure();
 
         ServerBusImpl serverBus = new ServerBusImpl();
-        //serverBus.setConsumerAndProducer("failover://tcp://192.168.99.100:61616");
-        serverBus.setConsumerAndProducer("failover://tcp://localhost:61616");
+        serverBus.setConsumerAndProducer(dockerPort);
         serverBus.addHandler(it -> it + " world", String.class, String.class);
 
         ClientBusImpl clientBus = new ClientBusImpl();
-        //clientBus.setConsumerAndProducer("failover://tcp://192.168.99.100:61616");
-        clientBus.setConsumerAndProducer("failover://tcp://localhost:61616");
+        clientBus.setConsumerAndProducer(dockerPort);
         CompletableFuture<String> future = clientBus.sendRequest("hello", String.class);
 
         String response = future.get();
