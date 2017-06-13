615da9b42e2b98751e6e7536a8532fc165a0cbc8
Filocha <pafo@gft.com>
2017-06-13 10:18:19 +0200

    Add functionality for sending emails

--- src/main/java/com/filocha/email/EmailSender.java
+++ src/main/java/com/filocha/email/EmailSender.java
@@ -1,11 +1,51 @@
 package com.filocha.email;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
+
+import java.util.Properties;
 
 @Component
 public class EmailSender {
 
-    public void sendEmail(String userEmail, String auctionUrl) {
+    @Value("${spring.mail.username}")
+    private String email;
 
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    public void sendEmail(String userEmail, String auctionUrl) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(userEmail);
+        message.setSubject("test");
+        message.setText(auctionUrl);
+        emailSender.send(message);
+    }
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(email);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 }
---
+++ src/test/java/com/filocha/EmailSenderTest.java
@@ -0,0 +1,23 @@
+package com.filocha;
+
+
+import com.filocha.email.EmailSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmailSenderTest {
+
+    @Autowired
+    private EmailSender emailSender;
+
+    @Test
+    public void shouldSendEmail() {
+        emailSender.sendEmail("pawel.filocha@gmail.com", "test");
+
+    }
+}
