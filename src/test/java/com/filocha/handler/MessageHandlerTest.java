ded8b7955559846c1676d6b9f273e14112f01c03
Virtum <pawel.filocha@gmail.com>
2018-01-01 16:29:04 +0100

    Add test for message habdler, remove old test class with test for messaging

--- src/test/java/com/filocha/handler/MessageHandlerTest.java
+++ src/test/java/com/filocha/handler/MessageHandlerTest.java
@@ -1,12 +1,19 @@
 package com.filocha.handler;
 
 import com.filocha.messaging.client.ClientBusImpl;
+import com.filocha.messaging.messages.finder.ItemFinderRequestMessage;
+import com.filocha.messaging.messages.finder.ItemFinderResponseMessage;
+import lombok.SneakyThrows;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,6 +29,7 @@ public class MessageHandlerTest {
     @Autowired
     private MessageHandler messageHandler;
 
+    @SneakyThrows
     @Test
     public void shouldAddNewSubscription() {
         messageHandler.createHandlers();
@@ -29,7 +37,14 @@ public class MessageHandlerTest {
         ClientBusImpl clientBus = new ClientBusImpl();
         clientBus.setConsumerAndProducer(activeMqHost, responseQueue, requestQueue);
 
+        ItemFinderRequestMessage requestMessage = ItemFinderRequestMessage
+                .builder()
+                .item("testItem")
+                .email("testEmail")
+                .build();
 
+        CompletableFuture<ItemFinderResponseMessage> responseMessage = clientBus.sendRequest(requestMessage, ItemFinderRequestMessage.class);
+
+        assertEquals("Subscribed!", responseMessage.get().getResponse());
     }
-
 }
