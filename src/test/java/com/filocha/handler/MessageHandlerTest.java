aeba6501d8187895451eca94a61355cb9c0e7efe
Virtum <pawel.filocha@gmail.com>
2018-03-17 22:41:59 +0100

    Add test exclude for sonar

--- src/test/java/com/filocha/handler/MessageHandlerTest.java
+++ src/test/java/com/filocha/handler/MessageHandlerTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
- 
+
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
