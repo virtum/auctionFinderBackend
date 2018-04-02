505dcbd932ae8449d410b05ed9e7d4259a5217e4
Virtum <pawel.filocha@gmail.com>
2018-04-02 19:45:24 +0200

    Remove redundant tests, add new test for subscription cache

--- src/test/java/com/filocha/storage/SubscriptionCacheTest.java
+++ src/test/java/com/filocha/storage/SubscriptionCacheTest.java
@@ -2,7 +2,6 @@ package com.filocha.storage;
 
 import com.filocha.finder.AuctionFinderImpl;
 import com.filocha.finder.RequestModel;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.ReplaySubject;
 import org.junit.Test;
@@ -55,6 +54,35 @@ public class SubscriptionCacheTest {
         subscriptions.onNext(Model.createNewSubscription(email, item));
 
         // emit events with found urls
+        subscriptions.onNext(Model.createModelForUpdate(email, item, new ArrayList<>(Collections.singletonList("url1"))));
+        subscriptions.onNext(Model.createModelForUpdate(email, item, new ArrayList<>(Collections.singletonList("url2"))));
+
+        // then
+        final SubscriberModel subscriber = repository
+                .skip(2) // skip two emitted events
+                .blockingFirst();
+
+        assertEquals(2, subscriber.getAuctions().get(0).getUrls().size());
+    }
+
+    @Test
+    public void shouldNotSaveSameUrlsTwice() {
+        // given
+        final ReplaySubject<Model> subscriptions = ReplaySubject.create();
+        final ReplaySubject<SubscriberModel> repository = ReplaySubject.create();
+
+        new SubscriptionCache().startCache(subscriptions, PublishSubject.create(), new AuctionFinderImpl(), repository,
+                PublishSubject.create());
+
+        final String email = "user@email";
+        final String item = "item";
+
+        // when
+        // create new subscription
+        subscriptions.onNext(Model.createNewSubscription(email, item));
+
+        // emit events with found urls
+        subscriptions.onNext(Model.createModelForUpdate(email, item, new ArrayList<>(Collections.singletonList("url1"))));
         subscriptions.onNext(Model.createModelForUpdate(email, item, new ArrayList<>(Collections.singletonList("url1"))));
         subscriptions.onNext(Model.createModelForUpdate(email, item, new ArrayList<>(Collections.singletonList("url2"))));
 
