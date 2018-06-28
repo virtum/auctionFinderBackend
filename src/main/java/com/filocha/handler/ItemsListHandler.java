a4990fba28975d22d35ab3e7ad4088b4666ac641
Virtum <pawel.filocha@gmail.com>
2018-06-28 08:00:01 +0200

    Code refactor

--- src/main/java/com/filocha/handler/ItemsListHandler.java
+++ src/main/java/com/filocha/handler/ItemsListHandler.java
@@ -4,14 +4,13 @@ import com.filocha.messaging.messages.subscriptions.Subscription;
 import com.filocha.messaging.messages.subscriptions.SubscriptionsRequestModel;
 import com.filocha.messaging.messages.subscriptions.SubscriptionsResponseModel;
 import com.filocha.storage.RepositoryExtensions;
-import com.filocha.storage.SubscriberModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class ItemsListHandler {
@@ -20,17 +19,19 @@ public class ItemsListHandler {
     private MongoTemplate mongoTemplate;
 
     public SubscriptionsResponseModel handleMessage(SubscriptionsRequestModel message) {
-        final Optional<SubscriberModel> subscriber = RepositoryExtensions.findSubscriber(mongoTemplate, message.getEmail());
-
-        final List<Subscription> items = new ArrayList<>();
-        subscriber.ifPresent(sub -> sub
-                .getAuctions()
-                .forEach(item -> items.add(Subscription
-                        .builder()
-                        .numberOfFoundItems(item.getUrls().size())
-                        .itemName(item.getItemName())
-                        .creationDate(item.getCreationDate())
-                        .build())));
+        final List<Subscription> items = RepositoryExtensions
+                .findSubscriber(mongoTemplate, message.getEmail())
+                .map(sub -> sub
+                        .getAuctions()
+                        .stream()
+                        .map(item -> Subscription
+                                .builder()
+                                .numberOfFoundItems(item.getUrls().size())
+                                .itemName(item.getItemName())
+                                .creationDate(item.getCreationDate())
+                                .build())
+                        .collect(Collectors.toList()))
+                .orElse(Collections.emptyList());
 
         return SubscriptionsResponseModel
                 .builder()
