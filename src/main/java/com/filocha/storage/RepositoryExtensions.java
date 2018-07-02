28020edfa97bef0dd425c8d18ef3e7a1c90d2648
Virtum <pawel.filocha@gmail.com>
2018-07-02 07:12:50 +0200

    Add javadoc for RepositoryExtension

--- src/main/java/com/filocha/storage/RepositoryExtensions.java
+++ src/main/java/com/filocha/storage/RepositoryExtensions.java
@@ -12,6 +12,12 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class RepositoryExtensions {
 
+    /**
+     * Returns observable stream which will be used to handle every subscriber update and stores it in database.
+     *
+     * @param mongoTemplate instance of mongo
+     * @return observable stream to handle subscriber update
+     */
     public static PublishSubject<SubscriberModel> updateSubscriber(final MongoTemplate mongoTemplate) {
         final PublishSubject<SubscriberModel> subject = PublishSubject.create();
 
@@ -22,10 +28,23 @@ public class RepositoryExtensions {
         return subject;
     }
 
+    /**
+     * Finds specific subscriber with subscriptions in database by given email.
+     *
+     * @param mongoTemplate instance of mongo
+     * @param email         subscriber email
+     * @return if non-null, Optional with SubscriberModel, otherwise returns an empty Optional
+     */
     public static Optional<SubscriberModel> findSubscriber(final MongoTemplate mongoTemplate, final String email) {
         return Optional.ofNullable(mongoTemplate.findOne(Query.query(Criteria.where("email").is(email)), SubscriberModel.class));
     }
 
+    /**
+     * Returns list of all subscribers with subscriptions in database.
+     *
+     * @param mongoTemplate instance of mongo
+     * @return list of all subscribers with subscriptions
+     */
     public static List<SubscriberModel> getAllSubscribers(final MongoTemplate mongoTemplate) {
         return new CopyOnWriteArrayList<>(mongoTemplate.findAll(SubscriberModel.class));
     }
