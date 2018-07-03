1747e6781f47e2a97af0ed19adf290c960ee14ba
Filocha <pafo@gft.com>
2018-07-03 17:17:40 +0200

    Remove redundant tests

--- src/test/java/com/filocha/throttle/ThrottleGuardTest.java
+++ src/test/java/com/filocha/throttle/ThrottleGuardTest.java
@@ -2,7 +2,6 @@ package com.filocha.throttle;
 
 import com.filocha.finder.RequestModel;
 import io.reactivex.Observable;
-import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.ReplaySubject;
 import org.junit.Test;
@@ -10,7 +9,6 @@ import org.junit.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static io.reactivex.Flowable.just;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ThrottleGuardTest {
@@ -82,83 +80,4 @@ public class ThrottleGuardTest {
 
         assertThat(results).containsExactly(model1, model2, model3, model4);
     }
-
-    @Test
-    public void tempObserveOnJust() {
-        just("Some String")
-                .map(it -> { // main
-                    System.out.println("Map2 thread: " + Thread.currentThread().getName());
-                    return it;
-                })
-                .observeOn(Schedulers.computation())
-                .subscribe(number -> System.out.println("Subscribe thread: " + Thread.currentThread().getName())); // computation
-    }
-
-    @Test
-    public void tempSubscribeOnJust() throws InterruptedException {
-        just("Some String")
-                .map(it -> { // computation
-                    System.out.println("Map2 thread: " + Thread.currentThread().getName());
-                    return it;
-                })
-                .subscribeOn(Schedulers.computation())
-                .subscribe(number -> System.out.println("Subscribe thread: " + Thread.currentThread().getName())); // computation
-
-        Thread.sleep(300);
-    }
-
-    @Test
-    public void shouldProcessOnMainThread() {
-        final PublishSubject<String> source = PublishSubject.create();
-        throttle(source) // main
-                .map(it -> {
-                    System.out.println("Map thread: " + Thread.currentThread().getName()); // main
-                    return it;
-                })
-                .subscribe(it -> System.out.println("Subscribe thread: " + Thread.currentThread().getName())); // main
-
-        source.onNext("1");
-    }
-
-
-    @Test
-    public void shouldSwitchThreadForSubscribeOnly() {
-        final PublishSubject<String> source = PublishSubject.create();
-        throttle(source) // main
-                .map(it -> {
-                    System.out.println("Map thread: " + Thread.currentThread().getName()); // main
-                    return it;
-                })
-                .observeOn(Schedulers.computation())
-                .subscribe(it -> System.out.println("Subscribe thread: " + Thread.currentThread().getName())); // computation
-
-        source.onNext("1");
-    }
-
-    @Test
-    public void shouldSwitchThreads() throws InterruptedException {
-        final PublishSubject<String> source = PublishSubject.create();
-        throttle(source) // main
-                .map(it -> {
-                    System.out.println("Map thread: " + Thread.currentThread().getName());
-                    return it;
-                })
-                .subscribeOn(Schedulers.computation())
-                .subscribe(it -> System.out.println("Subscribe thread: " + Thread.currentThread().getName())); // ???
-
-        source.onNext("1");
-
-        Thread.sleep(3000);
-    }
-
-    private PublishSubject<String> throttle(final PublishSubject<String> source) {
-        final PublishSubject<String> output = PublishSubject.create();
-
-        source.subscribe(output);
-        output.subscribe(it -> System.out.println("Throttle thread: " + Thread.currentThread().getName()));
-
-        return output;
-    }
-
-
 }
